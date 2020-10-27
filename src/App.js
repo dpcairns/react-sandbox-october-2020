f53c3ae29a6ae39eacac754559e0486b2e45a424
harriet-the-spy <dpcairns@gmail.com>
2020-10-27 10:50:57 -0700

    add counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
+  state = {
+    boogerCounter: 0,
+  }
+
+  handleBoogerClick = (e) => {
+    this.setState({ 
+      boogerCounter: this.state.boogerCounter + 1 
+    })
+  }
+
   render() {
       return (
         <div className="container">
-          <h1>It's react time!</h1>
-          <div className="flex">
-            Do react stuff here.
+            <h1>Click the button to increment the counter</h1>
+            <div className="flex">
+            <button onClick={this.handleBoogerClick}>
+              Click to count
+            </button>
+            <div>
+              Current count: {this.state.boogerCounter}
+            </div>
           </div>
         </div>
       );

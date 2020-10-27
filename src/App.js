83fc4ae7742c6ce96ec0e6a45a20a3dad34eb82a
harriet-the-spy <dpcairns@gmail.com>
2020-10-27 09:58:38 -0700

    derive from data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MyAmazingArticle from './MyAmazingArticle.js';
 import MySpecialFooter from './MySpecialFooter.js';
 import MySpecialHeader from './MySpecialHeader.js';
+import articles from './articles.js';
 
 export default class App extends React.Component {
   render() {
@@ -10,10 +11,10 @@ export default class App extends React.Component {
         <div>
         <h1>Time to click the legos together!</h1>
           <MySpecialHeader name="dani" />
-          <MyAmazingArticle caption="hi i am thirsty wolf"/>
-          <MyAmazingArticle caption="hi i am hungry wolf" />
-          <MyAmazingArticle caption="hi i am bad wolf" />
-          <MyAmazingArticle caption="hi i am good wolf"/>
+          <MyAmazingArticle caption={articles[0].title} color={articles[0].background} />
+          <MyAmazingArticle caption={articles[1].title} color={articles[1].background} />
+          <MyAmazingArticle caption={articles[2].title} color={articles[2].background} />
+          <MyAmazingArticle caption={articles[3].title} color={articles[3].background} />
           <MySpecialFooter phone="345-456-5431" />
         </div>
       );

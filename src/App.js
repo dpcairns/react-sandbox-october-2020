f66a70a019a7c34db7f3ec624a3202d8e6f6f716
harriet-the-spy <dpcairns@gmail.com>
2020-10-27 10:21:09 -0700

    add plain branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import MySpecialFooter from './MySpecialFooter.js';
-import MySpecialHeader from './MySpecialHeader.js';
-import ArticleList from './ArticleList.js';
 
 export default class App extends React.Component {
   render() {
       return (
-        <div className="grid-stuff">
-          <h1>Time to click the legos together!</h1>
-          <MySpecialHeader name="dani" />
-          <ArticleList />
-          <MySpecialFooter phone="345-456-5431" />
+        <div>
+          <h1>It's react time!</h1>
         </div>
       );
   }
--- src/MySpecialFooter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class MySpecialFooter extends React.Component {
-    render() {
-        return (
-            <div> I am the footer! Call us at {this.props.phone}</div>
-        );
-    }
-  }

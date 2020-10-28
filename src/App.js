81d11a739adb5e84feeb3b032b61d48728c36698
harriet-the-spy <dpcairns@gmail.com>
2020-10-28 11:16:19 -0700

    implement callback

--- src/App.js
+++ src/App.js
@@ -3,16 +3,36 @@ import './App.css';
 import MySpecialFooter from './MySpecialFooter.js';
 import MySpecialHeader from './MySpecialHeader.js';
 import WolfList from './WolfList.js';
+import DropDown from './DropDown.js';
+
 import wolves from './wolves.js';
 
+// 0) move DropDown into App.js
+// 1) move filter state to App.js
+// 2) give DropDown the debit card to change App.js state
+// 3) give WolfList the filter (since it's the component that needs it)
+
+// app.js is a container component. it manages state and passes it to children.
 export default class App extends React.Component {
+  state = {
+    filter: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      filter: e.target.value
+    });
+  }
+
   render() {
       return (
         <div className="grid-stuff">
           <h1>Time to click the legos together!</h1>
+
           <MySpecialHeader name="dani" />
-          <WolfList wolves={wolves}/>
+          <WolfList wolves={wolves} filter={this.state.filter}/>
           <MySpecialFooter phone="345-456-5431" />
+          <DropDown handleChange={this.handleChange} />
         </div>
       );
   }
--- src/WolfList.js
+++ src/WolfList.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react'
 import MyAmazingArticle from './MyAmazingArticle.js';
 
+// presentational component -- no state, just takes in props and display them
 export default class WolfList extends Component {
-  state = {
-    filter: ''
-  }
-
-  handleChange = e => {
-    this.setState({
-      filter: e.target.value
-    });
-  }
-
     render() {
         const filteredWolves = this.props.wolves.filter((wolf) => {
           // code golf: this.props.wolves.filter((wolf) => !this.state.filter || (wolf.size === this.state.filter))
-          if (!this.state.filter) return true;
+          if (!this.props.filter) return true;
 
-          if (wolf.size === this.state.filter) return true;
+          if (wolf.size === this.props.filter) return true;
 
           return false
         });
 
         return (
           <>
-            <select onChange={this.handleChange}>
-              <option value=''>Show All</option>
-              <option value='small'>Small</option>
-              <option value='medium'>Medium</option>
-              <option value='large'>Large</option>
-            </select>
             <div className="articles">
           {
             filteredWolves.map(wolf => 
